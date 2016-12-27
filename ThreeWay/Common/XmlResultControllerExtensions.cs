df13ffce3e3c03a364e61a9d568954d62e04b654
C. J. Anderson <chris@standsure.io>
2016-12-27 08:32:46 -0500

    add XmlResult as an extension method on Controller

---
+++ ThreeWay/Common/XmlResultControllerExtensions.cs
@@ -0,0 +1,22 @@
+﻿// //-----------------------------------------------------------------------
+// // <copyright file="XmlResultControllerExtensions.cs" company="Stand Sure LLC">
+// //     Copyright (c) 2016. All rights reserved.
+// // </copyright>
+// //-----------------------------------------------------------------------
+
+namespace ThreeWay.Common
+{
+  using System.Web.Mvc;
+  using System.Xml.Serialization;
+
+  public static class XmlResultControllerExtensions
+  {
+    public static XmlResult XML(
+      this Controller controller,
+      object model,
+      XmlAttributeOverrides xmlAttributeOverrides = null)
+    {
+      return new XmlResult(model, xmlAttributeOverrides);
+    }
+  }
+}
